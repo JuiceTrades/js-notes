d4acbf94ff624a16d8a3b8cfcb2101c214556018
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-01-13 13:07:10 -0500

    Initial notes commit

---
+++ js-slides.js
@@ -0,0 +1,6 @@
+var one = 1
+var two = 2 
+
+var me = {firstName: "Domitrius", lastName: "Clark", birthDay: "April 3, 1993", interests: ["League of Legends","OverWatch", "Web Development"] }
+
+alert(me.firstName+ " " + me.lastName)
